61ca4737b8cf1185223193742932d6d35a45083e
MohamedMF2 <furious_fouad@yahoo.com>
2019-09-26 15:56:15 +0300

    class rectangle defination  and creating an object in the main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include "car.h" ;
+#include "rectanglar.h" ;
+
 using namespace std ;
 
 int main () {
 	car c1;
 	c1.setMaker(" Nissan Skyline ") ;
 	c1.setModel ( 1992) ;
-
+	/*
 	cout << " the car is made by " << c1.getMaker() <<endl ;
 	cout << " at  " << c1.getModel() <<endl ;
+	*/
+	 rectanglar rec ; 
+	 rec.setLength(10.5);
+	 rec.setWidth(5.5);
+
+	 cout << " the rectanglar length is = " << rec.getLength() <<endl ;
+	cout << " the rectanglar Width is = " << rec.getWidth() <<endl ;
+	cout << " the area = " << rec.getLength() *   rec.getWidth() << endl ;
+
 }
---
+++ rectanglar.cpp
@@ -0,0 +1,29 @@
+#include "rectanglar.h"
+
+
+rectanglar::rectanglar(void)
+{
+}
+
+
+rectanglar::~rectanglar(void)
+{
+}
+
+// length Setter and Getter 
+void rectanglar::setLength ( float L){
+	length = L ;
+}
+
+float rectanglar::getLength () {
+	return length ;
+}  
+
+// Width Setter and Getter 
+void rectanglar::setWidth ( float W){
+	width  = W ;
+}
+
+float rectanglar::getWidth  () {
+	return width ;
+}
